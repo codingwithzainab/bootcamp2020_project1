24a31f995bc8812cbbe26440133b3258c04b8e87
codingwithzainab <codingwithzainabmunir@gmail.com>
2022-06-16 18:20:03 +0500

    iterates

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src='images/d.jpg' className="App-logo" alt="logo" />
+        <p> hi </p>
 
         <p>
          rejagnikotrjgy;pkjhoseikdhpkj
          rskljjngmtrjngj
          sjnmbjmgkl br
+
          nnfdsnjnm  
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src="./images/d.jpg" alt="err21or"></img>
         <a
           className="App-link"
           href="https://reactjs.org"
