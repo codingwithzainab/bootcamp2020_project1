5397ee8eedfe56b5d3e6037e68a2e7c5769ee57e
codingwithzainab <codingwithzainabmunir@gmail.com>
2022-06-16 18:03:54 +0500

    first project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src='images/d.jpg' className="App-logo" alt="logo" />
+
         <p>
+         rejagnikotrjgy;pkjhoseikdhpkj
+         rskljjngmtrjngj
+         sjnmbjmgkl br
+         nnfdsnjnm  
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
