b673f005f76e01a29aba842b8139e5b95d93287f
codingwithzainab <codingwithzainabmunir@gmail.com>
2022-06-17 10:57:05 +0500

    printed hellow world

--- src/App.js
+++ src/App.js
@@ -4,28 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> hi </p>
-
-        <p>
-         rejagnikotrjgy;pkjhoseikdhpkj
-         rskljjngmtrjngj
-         sjnmbjmgkl br
-
-         nnfdsnjnm  
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img src="./images/d.jpg" alt="err21or"></img>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hellow world from of all us</h1>
+ 
     </div>
   );
 }
